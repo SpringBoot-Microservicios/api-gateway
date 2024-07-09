a0f357e53e3171bec7adcfa3faadc765fd3b6516
orlandorocahuapaya <132851982+orlandorocahuapaya@users.noreply.github.com>
2024-07-09 15:41:10 -0500

    Add files via upload

---
+++ gateway/src/main/java/com/debuggeandoideas/gateway/dto/TokenDto.java
@@ -0,0 +1,14 @@
+package com.debuggeandoideas.gateway.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class TokenDto {
+
+    private String accessToken;
+
+}
